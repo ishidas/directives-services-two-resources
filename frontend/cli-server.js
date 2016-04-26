2a2d2facf43f19e0f57a65e707f1fe8ab926fe38
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-25 22:31:50 -0700

    added files and dependencies

---
+++ frontend/cli-server.js
@@ -0,0 +1,7 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+
+app.listen(8080, ()=>console.log('Cli Server listening at port 8080..'));
